a4a0d69828c4a43efeb3da4c926c8b355c8c0a76
nafi_pantha <nafpantha@gmail.com>
2020-06-07 01:10:08 +0600

    First Commit

---
+++ SimpleScan/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SimpleScan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroUserControl1 = new MetroFramework.Controls.MetroUserControl();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroUserControl1
+            // 
+            this.metroUserControl1.Location = new System.Drawing.Point(296, 299);
+            this.metroUserControl1.Name = "metroUserControl1";
+            this.metroUserControl1.Size = new System.Drawing.Size(150, 150);
+            this.metroUserControl1.TabIndex = 0;
+            this.metroUserControl1.UseSelectable = true;
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Location = new System.Drawing.Point(30, 82);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(309, 29);
+            this.metroComboBox1.TabIndex = 1;
+            this.metroComboBox1.UseSelectable = true;
+            this.metroComboBox1.SelectedIndexChanged += new System.EventHandler(this.metroComboBox1_SelectedIndexChanged);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(0, 0);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "metroButton1";
+            this.metroButton1.UseSelectable = true;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(64, 223);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(163, 50);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "Scan";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 600);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.metroUserControl1);
+            this.Name = "Form1";
+            this.Text = "Simple Scan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroUserControl metroUserControl1;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
+
---
+++ SimpleScan/Form1.cs
@@ -0,0 +1,175 @@
+﻿using NTwain;
+using NTwain.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SimpleScan
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+
+        TwainSession _twain;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var appId = TWIdentity.CreateFromAssembly(DataGroups.Image, Assembly.GetEntryAssembly());
+            _twain = new TwainSession(appId);
+            if (_twain.State < 3)
+            {
+                // use this for internal msg loop
+                _twain.Open();
+                // use this to hook into current app loop
+                //_twain.Open(new WindowsFormsMessageLoopHook(this.Handle));
+            }
+
+            Console.WriteLine("State" + _twain.State);
+
+            foreach (var src in _twain)
+            {
+                Console.WriteLine("Source"+ src.Name);
+            }
+
+            FillComboBox();
+        }
+
+        //protected override void OnHandleCreated(EventArgs e)
+        //{
+        //    base.OnHandleCreated(e);
+        //    SetupTwain();
+
+        //}
+
+
+        private void FillComboBox()
+        {
+            Console.Write("Yo");
+
+            Dictionary<int, string> spacecship = new Dictionary<int, string>();
+
+            int index = 1;
+
+            foreach (var src in _twain)
+            {
+                spacecship[index] = src.Name;
+                Console.WriteLine("Source" + src.Name);
+                index++;
+            }
+
+            //spacecship[1] = "Enterprise";
+            //spacecship[2] = "Spitzer";
+            //spacecship[3] = "WMAP";
+            //spacecship[4] = "Spitzer";
+            //spacecship[5] = "Casini";
+
+            BindingSource bindingSource = new BindingSource(spacecship, null);
+            metroComboBox1.DataSource = bindingSource;
+            metroComboBox1.DisplayMember = "Value";
+            metroComboBox1.ValueMember = "Value";
+        }
+
+        private void SetupTwain()
+        {
+            //var appId = TWIdentity.CreateFromAssembly(DataGroups.Image, Assembly.GetEntryAssembly());
+            //_twain = new TwainSession(appId);
+            _twain.StateChanged += (s, e) =>
+            {
+                PlatformInfo.Current.Log.Info("State changed to " + _twain.State + " on thread " + Thread.CurrentThread.ManagedThreadId);
+            };
+            _twain.TransferError += (s, e) =>
+            {
+                PlatformInfo.Current.Log.Info("Got xfer error on thread " + Thread.CurrentThread.ManagedThreadId);
+            };
+            _twain.DataTransferred += (s, e) =>
+            {
+                PlatformInfo.Current.Log.Info("Transferred data event on thread " + Thread.CurrentThread.ManagedThreadId);
+
+                // example on getting ext image info
+                var infos = e.GetExtImageInfo(ExtendedImageInfo.Camera).Where(it => it.ReturnCode == ReturnCode.Success);
+                foreach (var it in infos)
+                {
+                    var values = it.ReadValues();
+                    PlatformInfo.Current.Log.Info(string.Format("{0} = {1}", it.InfoID, values.FirstOrDefault()));
+                    break;
+                }
+
+                Image img = null;
+                if (e.NativeData != IntPtr.Zero)
+                {
+                    var stream = e.GetNativeImageStream();
+                    if (stream != null)
+                    {
+                        var outPut = StreamToByte(stream);
+                        //foreach (var socket in allSockets.ToList())
+                        //{
+                        //    socket.Send(outPut);
+                        //}
+                    }
+                }
+                else if (!string.IsNullOrEmpty(e.FileDataPath))
+                {
+                    img = new Bitmap(e.FileDataPath);
+                }
+
+            };
+            _twain.SourceDisabled += (s, e) =>
+            {
+                PlatformInfo.Current.Log.Info("Source disabled event on thread " + Thread.CurrentThread.ManagedThreadId);
+            };
+            _twain.TransferReady += (s, e) =>
+            {
+                PlatformInfo.Current.Log.Info("Transferr ready event on thread " + Thread.CurrentThread.ManagedThreadId);
+                //e.CancelAll = _stopScan;
+            };
+
+            // either set sync context and don't worry about threads during events,
+            // or don't and use control.invoke during the events yourself
+            PlatformInfo.Current.Log.Info("Setup thread = " + Thread.CurrentThread.ManagedThreadId);
+            _twain.SynchronizationContext = SynchronizationContext.Current;
+            if (_twain.State < 3)
+            {
+                // use this for internal msg loop
+                _twain.Open();
+                // use this to hook into current app loop
+                //_twain.Open(new WindowsFormsMessageLoopHook(this.Handle));
+            }
+        }
+
+        public static byte[] StreamToByte(Stream input)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+
+
+        private void metroComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var src = metroComboBox1.SelectedItem as DataSource;
+            //var src = _twain.CurrentSource;
+            Console.WriteLine("State: "+_twain.State);
+            Console.WriteLine("Source: "+src);
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("State: " + _twain.State);
+            SetupTwain();
+        }
+    }
+}
